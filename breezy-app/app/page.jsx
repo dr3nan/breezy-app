fab7e07ae52809c39bf72bef72c2d3c9eda895ff
Luke Saunders <lukensaunders@gmail.com>
2023-01-14 15:48:38 +0000

    feat midday commit

--- breezy-app/app/page.jsx
+++ breezy-app/app/page.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
-// import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
-// function Page() {
-// console.log(withPageAuthRequired)
-//   return (
-
-//       <div>HELLO YOU ARE HOME</div>
-
- 
-//   )
-// }
-
-// export default Page;
-
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function Profile() {
-  withPageAuthRequired()
   return (
   <div>Hello</div>
   )
 }
-
-// You can optionally pass your own `getServerSideProps` function into
-// `withPageAuthRequired` and the props will be merged with the `user` prop
 
--- breezy-app/app/profile.jsx
+++ breezy-app/app/profile.jsx
@@ -1,33 +1,34 @@
-'use client'
+'use client';
 
 // import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
-  console.log(user) 
+  console.log(user);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  
+
   if (user) {
     return (
       <div>
-        <h1> Welcome {user.name}!</h1>
-        <a href="/api/auth/logout">LOGOUT</a>
+        <img src={user.picture}/>
+        <h2>{user.nickname}</h2>
+        <p>{user.email}</p>
+        <a href='/api/auth/logout'>LOGOUT</a>
       </div>
-    )
+    );
   }
 
-  return <a href="/api/auth/login">LOGIN</a>
-  
-  
+  return <a href='/api/auth/login'>LOGIN</a>;
+
   return (
     user && (
       <div>
-            <img src={user.picture} alt={user.name} />
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </div>
-      )
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
   );
 }
