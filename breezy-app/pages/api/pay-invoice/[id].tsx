ca870db764b935b56190cc1d30339314780935b0
dr3nan <dolgarrenan@gmail.com>
2023-01-20 17:51:38 +0100

    fix: fixed request and response API methods

--- breezy-app/pages/api/pay-invoice/[id].tsx
+++ breezy-app/pages/api/pay-invoice/[id].tsx
@@ -1,7 +1,8 @@
 import connectMongo from '../../../utils/connectMongo';
 import Invoice from '../../../models/model';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await connectMongo();
   const { method } = req;
   const { id } = req.query;
@@ -11,7 +12,7 @@ export default async function handler(req, res) {
       try {
         const invoice = await Invoice.findOne({ purchaseOrderNumber: id });
         if (!invoice) {
-          return await res.status(400).json(invoice);
+          return res.status(400).json(invoice);
         }
         res.status(200).json(invoice);
         // res.send(invoice);
