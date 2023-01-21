b488fb37fcbe93c28ea13024aca12a919b14582b
dr3nan <dolgarrenan@gmail.com>
2023-01-21 18:21:05 +0100

    side buttons test unit working

--- breezy-app/cypress/e2e/cyE2E/membersOnly.cy.js
+++ breezy-app/cypress/e2e/cyE2E/membersOnly.cy.js
@@ -1,36 +1,12 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe('members-only', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('http://localhost:3000/members-only')
   })
 
   it('should display one sidebar', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
     cy.get('.sidebar_sidebar__1af5q').should('have.length', 1).should('be.visible')
-
-  //   // We can go even further and check that the default todos each contain
-  //   // the correct text. We use the `first` and `last` functions
-  //   // to get just the first and last matched elements individually,
-  //   // and then perform an assertion with `should`.
-  //   // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 
   it('should have a ul element with class styles.menuItems', () => {
@@ -53,6 +29,12 @@ describe('members-only', () => {
     cy.get('[data-cy="link-2"]').click()
     cy.wait(4000)
     cy.url().should('include', 'http://localhost:3000/members-only/clients')
+  })
+
+  it('if button LOGOUT clicked navigate to home OK', () => {
+    cy.get('.logout-button').click()
+    cy.wait(4000)
+    cy.url().should('include', 'http://localhost:3000/')
   })
 
   // it('can add new todo items', () => {
