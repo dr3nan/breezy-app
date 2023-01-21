95c59f02ca326fbfcc4f13b94f4d998159501234
dr3nan <dolgarrenan@gmail.com>
2023-01-21 16:59:01 +0100

    add: added sidebard items should be visible

--- breezy-app/cypress/e2e/cyE2E/membersOnly.cy.js
+++ breezy-app/cypress/e2e/cyE2E/membersOnly.cy.js
@@ -20,23 +20,27 @@ describe('members-only', () => {
     cy.visit('http://localhost:3000/members-only')
   })
 
-  it('should display one sidebar', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.sidebar_sidebar__1af5q').should('have.length', 1)
-    cy.get('.sidebar_sidebar__1af5q').should('be.visible')
+  // it('should display one sidebar', () => {
+  //   // We use the `cy.get()` command to get all elements that match the selector.
+  //   // Then, we use `should` to assert that there are two matched items,
+  //   // which are the two default items.
+  //   cy.get('.sidebar_sidebar__1af5q').should('have.length', 1)
+  //   cy.get('.sidebar_sidebar__1af5q').should('be.visible')
 
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
-  })
+  //   // We can go even further and check that the default todos each contain
+  //   // the correct text. We use the `first` and `last` functions
+  //   // to get just the first and last matched elements individually,
+  //   // and then perform an assertion with `should`.
+  //   // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  // })
 
-  it('should display three elements in the side bar', () => {
-    cy.get('.sidebar_sidebar__1af5q .sidebar_element').should('have-length', 3)
+  // it('should display three elements in the side bar', () => {
+  //   cy.get('.sidebar_menuItems__N7HT2 ul').should('have.length', 3)
 
+  // })
+
+  it('should have a ul element with class styles.menuItems', () => {
+    cy.get('.sidebar_menuItems__N7HT2').should('exist').should('be.visible')
   })
 
   // it('can add new todo items', () => {
