802e12366182d28428866dd886b7c801dcd37bb0
dr3nan <dolgarrenan@gmail.com>
2023-01-22 20:07:39 +0100

    chore: add extra testing to invoice-form

--- breezy-app/cypress/e2e/cyE2E/invoice-form.cy.js
+++ breezy-app/cypress/e2e/cyE2E/invoice-form.cy.js
@@ -32,10 +32,22 @@ describe('Create invoice', () => {
     cy.get('input[type="submit"][value="CREATE INVOICE"]').should('exist')
   })
 
-  it('should show an alert upon submit with PO number', () => {
+  it('should show an alert upon submit with PO number and reset form', () => {
     const stub = cy.stub();
     cy.on('window:alert', stub);
     cy.get('form').submit();
     cy.wrap(stub).should('be.called');
+    cy.getParamExists('fullName').should('have.value', '');
+    cy.getParamExists('address').should('have.value', '');
+    cy.getParamExists('phoneNumber').should('have.value', '');
+    cy.getParamExists('email').should('have.value', '');
+    cy.getParamExists('clientFullName').should('have.value', '');
+    cy.getParamExists('clientAddress').should('have.value', '');
+    cy.getParamExists('clientPhoneNumber').should('have.value', '');
+    cy.getParamExists('clientEmail').should('have.value', '');
+    cy.getParamExists('purchaseOrderNumber').should('have.value', '');
+    cy.getParamExists('description').should('have.value', '');
+    cy.getParamExists('rate').should('have.value', '');
+    cy.getParamExists('date').should('have.value', '');
   })
 })
