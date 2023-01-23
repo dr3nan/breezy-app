abb65e6b5c80e06a8ae6b9a133ca806109ccd177
Phil Cruz <mfel.cruz@gmail.com>
2023-01-23 17:15:31 +0100

    fix: added auth0 to members-only the rest does not work

--- breezy-app/app/layout.tsx
+++ breezy-app/app/layout.tsx
@@ -2,12 +2,15 @@
 
 import '../styles/globals.css';
 import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <head />
-      <body>{children}</body>
-    </html>
+    <UserProvider>
+      <html lang='en'>
+        <head />
+        <body>{children}</body>
+      </html>
+    </UserProvider>
   );
 };
--- breezy-app/app/members-only/page.tsx
+++ breezy-app/app/members-only/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import Logo from '../../public/LOGO 2.png';
+import Image from 'next/image';
 import style from '../../styles/sidebar.module.css';
 
-export default function MembersOnly() {
+export default withPageAuthRequired(function MembersOnly() {
   return (
     <div className={style.logoContainer}>
       <Image alt='breezy logo' src={Logo} width={600} />
     </div>
   );
-};
+});
+
+
